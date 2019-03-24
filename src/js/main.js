765d64dfd6b80576bcafefb41cc4398481b7f75d
Alexander <komarevich.alex@gmail.com>
2019-03-24 23:39:15 +0200

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import JonChart from './jonChart/common'
 
 document.addEventListener("DOMContentLoaded", () => {
-  var request = new Request("./chart_data.json");
+  var request = new Request("/chart_data.json");
   return fetch(request)
     .then(response => response.json())
     .then(data => {
